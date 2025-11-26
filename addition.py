ea159bd961dfda7d2b82165ae073f7bb095d928b
Satish H Vaishyar <12vsatish1@gmail.com>
2025-11-26 10:49:43 +0530

    Changed code

--- addition.py
+++ addition.py
@@ -5,9 +5,9 @@ def add(a, b):
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-        num1 = int(sys.argv[1])
-        num2 = int(sys.argv[2])
-        print("Sum: ", add(num1, num2))
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+        print("Sum: ", add(x, y))
     else:
         x = 10
         y = 20
